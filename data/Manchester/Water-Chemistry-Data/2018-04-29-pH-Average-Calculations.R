88875262aa0d356c7287c6ed501fa26d8e4014c4
Yaamini Venkataraman <yaaminiv@uw.edu>
2018-04-29 17:13:08 -0700

    copied information from project-oyster-oa

---
+++ data/Manchester/Water-Chemistry-Data/2018-04-29-pH-Average-Calculations.R
@@ -0,0 +1,28 @@
+#I will calculate average and standard error for pH from the discrete samples.
+
+#### IMPORT TOTAL ALKALINITY VALUES ####
+pHTanks <- read.csv("2018-04-29-pH-Discrete-Samples-by-Tank.csv", header = TRUE) #Import pH values
+head(pHTanks) #Confirm import
+
+#### AVERAGE VALUES ####
+samplingDays <- c(2, 2, 5, 5, 7, 7, 12, 12, 14, 14, 19, 19, 21, 21, 26, 26, 28, 28, 33, 33, 36, 36, 42, 42, 44, 44, 48, 48, 52, 52) #Isolate sampling days
+averagepH <- data.frame("Day" =  samplingDays,
+                        "Treatment" = rep(c("Experiment", "Control"), times = ((length(samplingDays))/2)),
+                        "averagepH" = rep(0, times = length(samplingDays)),
+                        "standardError" = rep(0, times = length(samplingDays))) #Create an empty dataframe
+head(averagepH) #Confirm dataframe creation
+npH <- as.numeric(length(averagepH$averagepH)) #Calculate length
+
+for(i in 1:npH){
+  averagepH$averagepH[i] <- mean(pHTanks$pH..mV.[((3*i)-2):(3*i)])
+} #Calculate means and add them to the table
+head(averagepH) #Confirm additions
+
+#### CALCULATE STANDARD ERROR ####
+for(i in 1:npH){
+  averagepH$standardError[i] <- sqrt(var(pHTanks$pH..mV.[((3*i)-2):(3*i)]))
+} #Calculate standard errors and add them to the table
+head(averagepH) #Confirm additions
+
+#### EXPORT DATA ####
+write.csv(averagepH, "2018-04-29-Average-pH.csv") #Export dataframe
---
+++ images/Manchester/Gigas-gonad-histology/2017-04-08-Sampling/README.md
@@ -0,0 +1,12 @@
+# Gigas-postOA-gonad-histology
+
+*C. gigas* gonad histology images from 2017-04-08 sampling. Oyster samples were collected after exposing individuals to low or ambient pH conditions. Images were taken by Grace Crandall. 
+
+From Grace: 
+
+- Each slide contains multiple individuals. 
+- Each individual has two images: one at high magnification (40x) and one at low magnification (4x). 
+- The images are labeled as: ID_hi/lo
+  - ID: oyster individual ID from [tissue sampling spreadsheet](https://github.com/RobertsLab/project-oyster-oa/blob/master/data/Manchester/2017-Adult-Gigas-Tissue-Sampling/20170408-GigasTissueSamplingInformation.xlsx)
+  - Magnification: "hi" = 40x; "lo" = 4x
+- NOTE: The histology images labeled with individual tissues as “UK-##” are currently not determined which oyster individual they belong to. During the histology process, it appears that the performers of the histology slides moved the tissues into new orientation from what Yaamini’s original key denotes the individual oysters.

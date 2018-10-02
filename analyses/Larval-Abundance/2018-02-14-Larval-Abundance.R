902c8678f0cac8dfb115e6470a8ff06e419b2d93
Yaamini Venkataraman <yaaminiv@uw.edu>
2018-10-01 17:55:47 -0700

    removed extra code from script

--- analyses/Larval-Abundance/2018-02-14-Larval-Abundance.R
+++ analyses/Larval-Abundance/2018-02-14-Larval-Abundance.R
@@ -14,25 +14,6 @@ hatchRatepHOnly$Male.Treatment <- factor(hatchRatepHOnly$Male.Treatment) #Make s
 hatchRatepHOnly <- hatchRatepHOnly[-24,] #Remove outlier from Ambient-Ambient group
 tail(hatchRatepHOnly) #Confirm removal
 
-#### ONE WAY ANOVA ####
-#hatchRatepHTreatmentANOVA <- aov(Average.Hatch.Rate ~ Parental.Treatment, data = hatchRatepHOnly) #One-way ANOVA by parental treatment
-#summary(hatchRatepHTreatmentANOVA)[[1]][["F value"]][[1]] #F = 3.10953
-#summary(hatchRatepHTreatmentANOVA)[[1]][["Pr(>F)"]][[1]] #p = 0.05082859
-
-#jpeg(filename = "analyses/Manchester_ReproductiveOutput_20180214/2018-03-07-Hatch-Rate-by-pH-Treatment-All-Groups.jpeg", width = 1500, height = 1000)
-#plot(x = hatchRatepHOnly$Parental.Treatment, y = hatchRatepHOnly$Average.Hatch.Rate, xlab = "Treatment", ylab = "Hatch Rate", main = "Hatch Rate by Treatment", cex.main = 4, cex.axis = 1, cex.lab = 1.4) #Preliminary plot
-#legend("topright", bty = "n", legend = paste("t =", format(sqrt(summary(hatchRatepHTreatmentANOVA)[[1]][["F value"]][[1]]), digits = 4), "p =", format(summary(hatchRatepHTreatmentANOVA)[[1]][["Pr(>F)"]][[1]], digits = 4))) #Add t and p-value
-#dev.off()
-
-#hatchRatepHTreatmentFemaleANOVA <- aov(Average.Hatch.Rate ~ Female.Treatment, data = hatchRatepHOnly) #One-way ANOVA by female treatment
-#sqrt(summary(hatchRatepHTreatmentFemaleANOVA)[[1]][["F value"]][[1]]) #t = 2.99445
-#summary(hatchRatepHTreatmentFemaleANOVA)[[1]][["Pr(>F)"]][[1]] #p = 0.006908886
-
-#jpeg(filename = "analyses/Manchester_ReproductiveOutput_20180214/2018-03-07-Hatch-Rate-by-pH-Treatment-Female-Groups.jpeg", width = 1500, height = 1000)
-#plot(x = hatchRatepHOnly$Female.Treatment, y = hatchRatepHOnly$Average.Hatch.Rate, xlab = "Treatment", ylab = "Hatch Rate", main = "Hatch Rate by Female Treatment", cex.main = 4, cex.axis = 1, cex.lab = 1.4) #Preliminary plot
-#legend("topright", bty = "n", legend = paste("t =", format(sqrt(summary(hatchRatepHTreatmentFemaleANOVA)[[1]][["F value"]][[1]]), digits = 4), "p =", format(summary(hatchRatepHTreatmentFemaleANOVA)[[1]][["Pr(>F)"]][[1]], digits = 4))) #Add t and p-value
-#dev.off()
-
 #### LINEAR MIXED EFFECT MODEL ####
 
 install.packages("lme4") #Install package for linear mixed effect models
@@ -73,7 +54,6 @@ qqline(residuals(hatchRatepHTreatment.lmer)) #Data falls on a straight line, so 
 #Linearity and homoskedasticity
 plot(fitted(hatchRatepHTreatment.lmer), residuals(hatchRatepHTreatment.lmer), xlab = "Fitted values", ylab = "Residuals")
 abline(h = 0)
-#Distributed across y-axis but not x-axis. Three groups present. Might be okay. No homoskedasticity.
 
 #Female.treatment
 
@@ -84,13 +64,12 @@ qqline(residuals(hatchRatepHTreatment.lmer2)) #Data falls on a straight line, so
 #Linearity and homoskedasticity
 plot(fitted(hatchRatepHTreatment.lmer2), residuals(hatchRatepHTreatment.lmer2), xlab = "Fitted values", ylab = "Residuals")
 abline(h = 0)
-#Distributed across y-axis but not x-axis. Two groups: low pH and ambient pH females. Might be okay. No homoskedasticity.
 
-#### MANCHESTER PAPER FIGURES ####
-#jpeg(filename = "analyses/Larval-Abundance//2018-04-16-Manchester-Paper-Figure.jpeg", width = 1500, height = 1000)
+#### PAPER FIGURES ####
+#jpeg(filename = "analyses/Larval-Abundance/2018-04-16-Manchester-Paper-Figure.jpeg", width = 1500, height = 1000)
 plot(x = hatchRatepHOnly$Parental.Treatment, y = hatchRatepHOnly$Average.Hatch.Rate, cex.axis = 2, col = "light grey") #Preliminary plot. Will modify in InDesign for publication
 #dev.off()
 
-#jpeg(filename = "analyses/Larval-Abundance//2018-07-30-Manchester-Paper-Figure2.jpeg", width = 1500, height = 1000)
+#jpeg(filename = "analyses/Larval-Abundance/2018-07-30-Manchester-Paper-Figure2.jpeg", width = 1500, height = 1000)
 plot(x = hatchRatepHOnly$Female.Treatment, y = hatchRatepHOnly$Average.Hatch.Rate, cex.axis = 2, col = "light grey") #Preliminary plot. Will modify in InDesign for publication
 #dev.off()
